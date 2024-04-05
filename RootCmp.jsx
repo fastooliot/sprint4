5abfc9bfb33e2f8e6e49c033842c6ffbaae61940
fastooliot <eladfast13@gmail.com>
2024-04-05 21:12:48 +0300

    Build nav bar

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,5 +1,22 @@
+const Router = ReactRouterDOM.HashRouter
+const {Route, Routes} = ReactRouterDOM
+const {useState, useEffect} = React
+
+import { AppNav } from './cmps/AppNav.jsx'
+// import { Home } from './pages/Home.jsx'
+// import { User } from './pages/User.jsx'
+// import { Notifications } from './pages/Notifications.jsx'
+// import { UserProfile } from './pages/UserProfile.jsx'
+// import { Messages } from './pages/Messages.jsx'
+// import { Reels } from './pages/Reels.jsx'
+// import { Explore} from './pages/Explore.jsx'
+
 export function RootCmp() {
-    return <div>
-        <h1>hello</h1>
-    </div>
+    const userLoggedIn = useState(false)
+
+    return <Router>
+        <section className='app main-layout'>
+            <AppNav />
+        </section>
+    </Router>    
 }
---
+++ cmps/AppNav.jsx
@@ -0,0 +1,45 @@
+export function AppNav() {
+    return <section className="app-nav-container">
+        <section className="app-nav-icon">
+            <img className="instagram-icon" src=".\assets\img\icons\instagram.svg" alt="Instagram logo" />
+        </section>
+        <section className="app-nav-options">
+            <section className="icon-couple">
+                <img className="icon" src=".\assets\img\icons\home.svg" alt="Home icon" />
+                <span className="icon-name">Home</span>
+            </section>
+            <section className="icon-couple">
+                <img className="icon" src=".\assets\img\icons\search.svg" alt="Home icon" />
+                <span className="icon-name">Search</span>
+            </section>
+            <section className="icon-couple">
+                <img className="icon" src=".\assets\img\icons\explore.svg" alt="Home icon" />
+                <span className="icon-name">Explore</span>
+            </section>
+            <section className="icon-couple">
+                <img className="icon" src=".\assets\img\icons\reels.svg" alt="Home icon" />
+                <span className="icon-name">Reels</span>
+            </section>
+            <section className="icon-couple">
+                <img className="icon" src=".\assets\img\icons\messages.svg" alt="Home icon" />
+                <span className="icon-name">Messages</span>
+            </section>
+            <section className="icon-couple">
+                <img className="icon" src=".\assets\img\icons\notifications.svg" alt="Home icon" />
+                <span className="icon-name">Notifications</span>
+            </section>
+            <section className="icon-couple">
+                <img className="icon" src=".\assets\img\icons\create.svg" alt="Home icon" />
+                <span className="icon-name">Create</span>
+            </section>
+            <section className="icon-couple">
+                <img className="icon" src=".\assets\img\icons\profile.svg" alt="Home icon" />
+                <span className="icon-name">Profile</span>
+            </section>
+        </section>
+        <section className="more-icon icon-couple">
+            <img className="icon" src=".\assets\img\icons\more.svg" alt="Home icon" />
+            <span className="icon-name">More</span>
+        </section>
+    </section>
+}
